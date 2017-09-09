5048d9abd060d590589f72fdb8177383590f3e35
Pia Kochar <piakochar11@gmail.com>
2017-09-09 00:19:00 -0400

    js

---
+++ icantread/static/tabs.js
@@ -0,0 +1,17 @@
+function switchTab(event, feedType) {
+	console.log(feedType);
+	var i, tabcontent, tablinks;
+
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    
+    document.getElementById(feedType).style.display = "block";
+    event.currentTarget.className += " active";
+}
