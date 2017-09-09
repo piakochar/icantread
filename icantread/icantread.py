377d449d301ffae4dd833a96a7cda1c1ad2ed358
Pia Kochar <piakochar11@gmail.com>
2017-09-08 22:56:34 -0400

    visual stuff

--- icantread/icantread.py
+++ icantread/icantread.py
@@ -14,5 +14,13 @@ def render_home():
     return render_template('home.html')
 
 @app.route('/', methods=['POST'])
+def upload():
+    return redirect(url_for('render_format_select'))
+
+@app.route('/ezread')
+def render_format_select():
+    return render_template('fun_reading.html', type='facebook')
+
+@app.route('/ezread', methods=['POST'])
 def make_reading_fun():
-    return render_template('fun_reading.html')
+    return render_template('feed.html')
